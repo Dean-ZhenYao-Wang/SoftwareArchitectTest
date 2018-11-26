47fcfc896b3a7abe90dd86439cc25c816f35ae44
ZhenYaoWang <zhenyao.wang@wzy.ren>
2018-11-27 00:19:41 +0800

    add UserConfig

---
+++ Services/ModelConfig/UserConfig.cs
@@ -0,0 +1,20 @@
+﻿using Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ModelConfig
+{
+    public class UserConfig:EntityTypeConfiguration<UserEntity>
+    {
+        public UserConfig()
+        {
+            ToTable("T_Users");
+            Property(p => p.LoginName).IsRequired().HasMaxLength(50);
+            Property(p => p.PassWord).IsRequired().HasMaxLength(20);
+        }
+    }
+}
