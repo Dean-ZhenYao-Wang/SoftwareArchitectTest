81a6905ecc2159d8fab7089cb1f7747c1d3a7ae1
ZhenYaoWang <zhenyao.wang@wzy.ren>
2018-11-27 00:24:00 +0800

    add CreditCardConfig

---
+++ Services/ModelConfig/CreditCardConfig.cs
@@ -0,0 +1,19 @@
+﻿using Services.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.ModelConfig
+{
+    public class CreditCardConfig:EntityTypeConfiguration<CreditCardEntity>
+    {
+        public CreditCardConfig()
+        {
+            ToTable("T_CreditCard");
+            Property(p => p.CardNumber).IsRequired().HasMaxLength(16);
+        }
+    }
+}
