60751b811fb43d11bf4ef679494ad67686d7863b
ZhenYaoWang <zhenyao.wang@wzy.ren>
2018-11-26 23:53:31 +0800

    An identification interface, which must be implemented by all services

---
+++ IServices/IServiceSupport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IServices
+{
+    /// <summary>
+    /// An identification interface, which must be implemented by all services
+    /// This ensures that only real service implementation classes will be registered with Autofac
+    /// </summary>
+    public interface IServiceSupport
+    {
+    }
+}
