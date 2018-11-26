7b6856f5d319f16b28d8be160f1ddd6c1374d288
ZhenYaoWang <zhenyao.wang@wzy.ren>
2018-11-26 23:58:58 +0800

    IUserServices

---
+++ IServices/IUserService.cs
@@ -0,0 +1,32 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IServices
+{
+    public interface IUserService:IServiceSupport
+    {
+        UserDTO GetById(Guid id);
+        /// <summary>
+        /// Check that the username password is correct
+        /// </summary>
+        /// <param name="loginName"></param>
+        /// <param name="passWord"></param>
+        /// <returns></returns>
+        bool CheckLogin(string loginName, string passWord);
+        /// <summary>
+        /// Record a login failure
+        /// </summary>
+        /// <param name="id">userId</param>
+        void IncrLoginError(Guid id);
+        /// <summary>
+        /// Determine whether the user has been locked
+        /// </summary>
+        /// <param name="id">userId</param>
+        /// <returns></returns>
+        bool IsLocked(long id);
+    }
+}
