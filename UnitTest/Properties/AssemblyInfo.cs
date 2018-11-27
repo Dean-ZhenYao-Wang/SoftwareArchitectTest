838e4b0fcf11def7835029bca04cc067c65cea07
ZhenYaoWang <zhenyao.wang@wzy.ren>
2018-11-27 10:01:21 +0800

    unittest

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("55df814d-233f-43d6-ba11-847836b20fd0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidVisa()
+        {
+        }
+        [TestMethod]
+        public void ValidMastercard()
+        {
+        }
+        [TestMethod]
+        public void ValidAmex()
+        {
+        }
+        [TestMethod]
+        public void ValidJcb()
+        {
+        }
+        [TestMethod]
+        public void InValidVisa()
+        {
+        }
+        [TestMethod]
+        public void InValidMastercard()
+        {
+        }
+        [TestMethod]
+        public void InValidAmex()
+        {
+        }
+        [TestMethod]
+        public void InValidJcb()
+        {
+        }
+    }
+}
